d3044cc44d225aea439260108eb5e4e35947cace
danil <mr.dansl@bk.ru>
2017-05-11 21:48:51 +0300

    js

---
+++ css/05.10/src/js/app.js
@@ -0,0 +1,193 @@
+
+
+
+/*var ask = +prompt("namber?");
+var ask2 = +prompt("namber2?");
+var quetion = prompt("add?");
+var a = quetion ? (ask + ask2) : (ask - ask2);
+
+alert (a);
+*/
+
+
+
+
+
+/*var ask = prompt("your marks?100-0");
+
+if(ask >= 95 && ask <= 100) {
+    alert("a")
+} else if (ask <=94 && ask >=85 ) {
+    alert("b")
+} else  if (ask <=84 && ask >=75) {
+    alert("c")
+} else  if (ask >=65 && ask <=75) {
+    alert("d")
+} else  if (ask >=60 && ask <=64) {
+    alert("e")
+} else if (ask >=0 && ask <=59) {
+    alert("fx")
+} else {
+    alert("error")
+}*/
+
+
+
+
+
+
+//var i = 0;
+//
+//do {
+// var leson = prompt("math , biologi , eles");
+//    var marks = prompt("your marks"+ leson);
+//    alert("good");
+//    alert("bad");
+//    i++;
+//} while(marks >= 61 && marks <= 100 || marks <=60 && marks >=0);
+
+
+
+
+
+//for (var i = 1; i <= 5; i++) {
+//    var lesson = prompt ("Lesson namr?");
+//    var mark = +prompt ("Your mark?");
+//    var bad = "bad mark! "+lesson;
+//
+//    if (mark >= 60) {
+//        bad = "У вас високі оцінки !!!";
+//    } else {
+//        break;
+//    }
+//}
+//
+//alert(bad);
+
+
+
+
+
+
+//for(var i = 0;i<=100;i+=2){
+//    console.log(i)
+//}
+
+
+
+
+
+//do {
+//    var first = +prompt("put first number");
+//    var second = +prompt("put second number");
+//    var choose = prompt("Your operation?");
+//    var result;
+//    switch(choose){
+//        case "*": {
+//            result = first * second;
+//               break;
+//               }
+//        case "/": {
+//               result = first / second;
+//               break;
+//               }
+//        case "+":{
+//               result = first + second;
+//                break;
+//                }
+//        case "-":{
+//                result = first - second;
+//                break;
+//                }
+//
+//        default:
+//                break;
+//            }
+//
+//        alert(result);
+//
+//    var repeat = confirm("repeat?");
+//
+//} while (repeat)
+
+
+
+
+
+
+
+//var month = prompt("tell your month?");
+//
+//switch(month) {
+//    case ("january"): {
+//        month = 1;
+//        break;
+//    }
+//
+//    case ("february"):{
+//        month = 2;
+//        break;
+//    }
+//
+//    case ("march"):{
+//        month = 3;
+//        break;
+//    }
+//
+//    case ("april"):{
+//        month = 4;
+//        break;
+//    }
+//
+//    case ("may"):{
+//        month = 5;
+//        break;
+//    }
+//
+//    case ("june"):{
+//        month = 6;
+//        break;
+//    }
+//
+//    case ("july "):{
+//        month = 7;
+//        break;
+//    }
+//
+//    case ("august"):{
+//        month = 8;
+//        break;
+//    }
+//
+//    case ("september "):{
+//        month = 9;
+//        break;
+//    }
+//
+//    case ("october"):{
+//        month = 10;
+//        break;
+//    }
+//
+//    case ("november"):{
+//        month = 10;
+//        break;
+//    }
+//
+//    case ("december"):{
+//        month = 12;
+//        break;
+//    }
+//}
+//
+//alert(month);
+
+
+
+
+//for (var i = 0;i<=30;i++){
+//   var math = Math.pow(2,i);
+//  console.log(math);
+//}
+
+
